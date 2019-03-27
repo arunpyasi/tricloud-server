8a87252c32e790f56788fd6d53d5f736d560d754
Arun Kumar Pyasi <expertaruncorp@gmail.com>
2019-03-28 01:36:27 +0545

    added agent and user routes

---
+++ rest_api/main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	fmt.Println("Welcome to TriCloud REST_API")
+	r := mux.NewRouter() // Here, r is router
+	agent = append(agent, Agent{ID: "1", Name: "Lenovo-B50", OS: "Ubuntu/Linux"})
+
+	r.HandleFunc("/users", GetUsers).Methods("GET")
+	r.HandleFunc("/users/{id}", GetUser).Methods("GET")
+	r.HandleFunc("/users/{id}", CreateUser).Methods("POST")
+	r.HandleFunc("/users/{id}", UpdateUser).Methods("PUT")
+	r.HandleFunc("/users/{id}", DeleteUser).Methods("DELETE")
+
+	r.HandleFunc("/agents", GetAgents).Methods("GET")
+	r.HandleFunc("/agent/{id}", GetAgent).Methods("GET")
+	r.HandleFunc("/agent/{id}", CreateAgent).Methods("POST")
+	r.HandleFunc("/agent/{id}", UpdateAgent).Methods("PUT")
+	r.HandleFunc("/agent/{id}", DeleteAgent).Methods("DELETE")
+
+	log.Fatal(http.ListenAndServe(":8000", r)) //listening and serving router
+}
+
+type Agent struct {
+	ID   string `json:"id,omitempty"`
+	Name string `json:"name,omitempty"`
+	OS   string `json:"os,omitempty"`
+}
+
+var agent []Agent
+
+func GetUsers(w http.ResponseWriter, r *http.Request)   {}
+func GetUser(w http.ResponseWriter, r *http.Request)    {}
+func CreateUser(w http.ResponseWriter, r *http.Request) {}
+func UpdateUser(w http.ResponseWriter, r *http.Request) {}
+func DeleteUser(w http.ResponseWriter, r *http.Request) {}
+
+func GetAgents(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+	json.NewEncoder(w).Encode(agent)
+}
+
+func GetAgent(w http.ResponseWriter, r *http.Request)    {}
+func CreateAgent(w http.ResponseWriter, r *http.Request) {}
+func UpdateAgent(w http.ResponseWriter, r *http.Request) {}
+func DeleteAgent(w http.ResponseWriter, r *http.Request) {}
