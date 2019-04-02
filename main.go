69faf3ea2cec27474349d8224c96cda9a37d6f91
bornjre <bornjre@gmail.com>
2019-04-02 09:08:28 +0545

    logging listen and serve

--- main.go
+++ main.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"log"
 	"net/http"
 
 	"github.com/indrenicloud/tricloud-server/broker"
 	"github.com/indrenicloud/tricloud-server/restapi"
+	"github.com/indrenicloud/tricloud-server/restapi/database"
 
 	"github.com/gorilla/mux"
 )
@@ -20,20 +22,20 @@ func main() {
 	r := mux.NewRouter()
 	restapi.RegisterAPI(r.PathPrefix("/api").Subrouter())
 	restapi.RegisterAuthHandlers(r.PathPrefix("/login").Subrouter())
-	//defer database.Close()
+	defer database.Close()
 
 	r.HandleFunc("/", rootRoute)
 
 	r.Handle("/public/", http.StripPrefix("/public/", http.FileServer(http.Dir("./public"))))
 	r.HandleFunc("/websocket", mBroker.ServeUserWebsocket)
-	http.ListenAndServe(":8080", r)
+	log.Println(http.ListenAndServe(":8080", r))
 
 }
 
 func listenAgentsConnection() {
 	mainRouter := mux.NewRouter()
 	mainRouter.HandleFunc("/websocket", mBroker.ServeAgentWebsocket)
-	http.ListenAndServe(":8081", mainRouter)
+	log.Println(http.ListenAndServe(":8081", mainRouter))
 
 }
 
